c31a8047ad9c13b7fc47d0629bd0bf36c466b15f
Denise Pingiori <pingiorid@gmail.com>
2024-07-14 17:42:10 +0200

    [upd] text styled

--- js/script.js
+++ js/script.js
@@ -21,3 +21,10 @@ const images = [
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
 ];
+
+//definisco le variabili
+let itemsContent = ''; //definisco la var che contiene le imgs nella sezione principale
+
+images.forEach((elem) => {
+    
+})
